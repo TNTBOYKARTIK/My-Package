be5405dfd6f55a1ddf3d4cc917a8bc59e53a0848
TNTBOYKARTIK <tntboykartik10@gmail.com>
2023-10-25 20:00:33 +0530

    changes

--- kartik/__init__.py
+++ kartik/__init__.py
@@ -1,18 +1,24 @@
 def add(a,b):
     print("The Sum of ",a," and ",b," is ",a+b)
     print("Made by TNTBOYKARTIK")
+    print("Thank you!")
 def sub(c,d):
     print("The Subtraction of ",c," and ",d," is ",c-d)
     print("Made by TNTBOYKARTIK")
+    print("Thank you!")
 def pro(e,f):
     print("The Product of ",e," and ",f," is ",e*f)
     print("Made by TNTBOYKARTIK")
+    print("Thank you!")
 def div(g,h):
     print("The Quotient of ",g," and ",h," is ",g/h)
     print("Made by TNTBOYKARTIK")
+    print("Thank you!")
 def mod(i,j):
     print("The Remainder of ",i," / ",j," is ",i%j)
     print("Made by TNTBOYKARTIK")
+    print("Thank you!")
 def exp(k,l):
     print("The exoponentation of ",k," and ",l," is ",k**l)
     print("Made by TNTBOYKARTIK")
+    print("Thank you!")
