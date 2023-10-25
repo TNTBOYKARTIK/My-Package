66429016be1a717bff6b4a9912b756b4769359e9
TNTBOYKARTIK <tntboykartik10@gmail.com>
2023-10-25 19:53:22 +0530

    first commit

---
+++ kartik/__init__.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    print("The Sum of ",a," and ",b," is ",a+b)
+    print("Made by TNTBOYKARTIK")
+def sub(c,d):
+    print("The Subtraction of ",c," and ",d," is ",c-d)
+    print("Made by TNTBOYKARTIK")
+def pro(e,f):
+    print("The Product of ",e," and ",f," is ",e*f)
+    print("Made by TNTBOYKARTIK")
+def div(g,h):
+    print("The Quotient of ",g," and ",h," is ",g/h)
+    print("Made by TNTBOYKARTIK")
+def mod(i,j):
+    print("The Remainder of ",i," / ",j," is ",i%j)
+    print("Made by TNTBOYKARTIK")
+def exp(k,l):
+    print("The exoponentation of ",k," and ",l," is ",k**l)
+    print("Made by TNTBOYKARTIK")
---
+++ kartik/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="kartik", 
+      version="0.1",
+      description="This is A simple Calculator Package by TNTBOYKARTIK",
+      author="Kartik",
+      packages=['kartik'],
+      install_requires=[])
